d44c08dd14d20147c2e67feda9673be5de9d6376
Marcelo Carrascal <155754494+mdcarrascal@users.noreply.github.com>
2024-01-06 16:52:56 -0300

    Add files via upload
    First upload

---
+++ wrangle-data.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+murders <-read_csv("data/murders.csv")
+head(murders)
+murders <- murders %>% mutate (region = factor(region), rate = total /population * 10^5)
+save(murders, file = "rda/murders.rda")
+head(murders)
